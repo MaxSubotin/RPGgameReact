0b6828414e0e42679e46bfccafc4298990cadd82
Maxim Subotin <73962386+MaxSubotin@users.noreply.github.com>
2021-06-25 21:06:01 +0300

    changing stuff

--- rpg-web-game/src/App.js
+++ rpg-web-game/src/App.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import Signin from './components/Signin';
-import Register from './components/Register';
-import Navbar from './components/Navbar';
-import ClassSelection from './components/ClassSelection';
-import BattleScene from './components/BattleScene';
-import EndScene from './components/EndScene';
-import './index.css';
+import { useState } from "react";
+import Signin from "./components/Signin";
+import Register from "./components/Register";
+import Navbar from "./components/Navbar";
+import ClassSelection from "./components/ClassSelection";
+import BattleScene from "./components/BattleScene";
+import EndScene from "./components/EndScene";
+import "./index.css";
 
 function App() {
   const [] = useState();
@@ -13,9 +13,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <Signin />
+      {/* <Signin /> */}
       {/* <Register /> */}
-      {/* <ClassSelection /> */}
+      <ClassSelection />
       {/* <BattleScene /> */}
       {/* <EndScene /> */}
     </>
