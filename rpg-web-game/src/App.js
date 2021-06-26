0c3edefeab32a7dbc3f0889298ef3ac834688d53
Maxim Subotin <73962386+MaxSubotin@users.noreply.github.com>
2021-06-26 18:02:05 +0300

    changed class selection layout and css

--- rpg-web-game/src/App.js
+++ rpg-web-game/src/App.js
@@ -15,8 +15,8 @@ function App() {
       <Navbar />
       {/* <Signin /> */}
       {/* <Register /> */}
-      <ClassSelection />
-      {/* <BattleScene /> */}
+      {/* <ClassSelection /> */}
+      <BattleScene />
       {/* <EndScene /> */}
     </div>
   );
--- rpg-web-game/src/components/ClassSelection.js
+++ rpg-web-game/src/components/ClassSelection.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React from "react";
+import "../index.css";
 
 function ClassSelection() {
   return (
     <div className="class-select-container">
       <h1 className="h1ChooseClass">Select Your Class</h1>
       <div className="class-select-table">
-        <div className="class">
-          <img alt="class image"></img>
-          <p>class description</p>
-          <p>class stats</p>
+        <div className="class warrior">
+          <ul className="class-ul">
+            <li className="class-li">STR: 12</li>
+            <li className="class-li">INT: 8</li>
+            <li className="class-li">DEX: 11</li>
+          </ul>
+          <p>
+            this is the warrior class, he can smack your brains in using his
+            bare hands
+          </p>
         </div>
-        <div className="class">
-          <img alt="class image"></img>
-          <p>class description</p>
-          <p>class stats</p>
+        <div className="class mage">
+          <ul className="class-ul">
+            <li className="class-li">STR: 12</li>
+            <li className="class-li">INT: 8</li>
+            <li className="class-li">DEX: 11</li>
+          </ul>
+          <p>
+            this is the warrior class, he can smack your brains in using his
+            bare hands
+          </p>
         </div>
-        <div className="class">
-          <img alt="class image"></img>
-          <p>class description</p>
-          <p>class stats</p>
+        <div className="class thief">
+          <ul className="class-ul">
+            <li className="class-li">STR: 12</li>
+            <li className="class-li">INT: 8</li>
+            <li className="class-li">DEX: 11</li>
+          </ul>
+          <p>
+            this is the warrior class, he can smack your brains in using his
+            bare hands
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ClassSelection
+export default ClassSelection;
